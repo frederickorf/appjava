53db84fcee104b83a2be8af71d09735229049bd5
Frederick Rodriguez <frederickorf@gmail.com>
2024-12-07 19:13:08 -0300

    03 - Strings 07-12-2024 - 1

--- src/Main.java
+++ src/Main.java
@@ -27,9 +27,14 @@ public class Main {
         System.out.println(texto.endsWith("undo!"));
         System.out.println(texto.startsWith("Ch"));
         System.out.println(texto.contains("und"));
+        System.out.println(texto.indexOf("Mund"));
 
+        System.out.println(texto.toUpperCase());
+        System.out.println(texto.toLowerCase());
 
-
+        String malo = "       chanchito feliz    ";
+        System.out.println(malo.trim());
+        System.out.println(malo);
 
     }
 }
