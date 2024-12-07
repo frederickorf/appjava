dd4466ccc6b9cd52179660802b31d389edaa0df7
Frederick Rodriguez <frederickorf@gmail.com>
2024-12-07 19:34:03 -0300

    05 - Arrays 07-12-2024 - 1

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         /*
@@ -16,10 +19,13 @@ public class Main {
 
          */
 
+        //03 - STRINGS
+        /*
         String text = "Hola Mundo!";
 
         int largo = text.length();
         System.out.println(largo);
+
 
         String texto = text.replace("Hola","Chao");
         System.out.println(text);
@@ -35,6 +41,30 @@ public class Main {
         String malo = "       chanchito feliz    ";
         System.out.println(malo.trim());
         System.out.println(malo);
+
+        */
+
+        // 04 - CARACTERES DE ESCAPE
+
+        /*
+
+        //String texto = "C:\\Hola \"Mundo\">";
+        //String texto = "Hola \nMundo";
+        String texto = "Hola \tMundo";
+        System.out.println(texto);
+
+        */
+
+        // 05 - ARRAYS
+
+        //int[] numeros = new int[5];
+        int[] numeros = {0,1,2,3,4};
+        numeros[0] = 5;
+        numeros[4] = 7;
+        System.out.println(Arrays.toString(numeros));
+        System.out.println(numeros.length);
+        Arrays.sort(numeros);
+        System.out.println(Arrays.toString(numeros));
 
     }
 }
