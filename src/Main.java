68e09a7cbe78b4bcc425eab652d412cf6be6dcf9
Frederick Rodriguez <frederickorf@gmail.com>
2024-12-06 21:22:56 -0300

    03 - Strings 12-06-2024 - 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!");
+        /*
+
+        byte edad = 18;
+        float age = 18.5f;
+        int numeroGrande = 1000;
+        System.out.println(edad);
+
+        boolean isActive = true;
+
+        char curso = 'A';
+
+        System.out.println(isActive);
+        System.out.println(curso);
+
+         */
+
+        String text = "Hola Mundo!";
+
+        int largo = text.length();
+        System.out.println(largo);
+
+        String texto = text.replace("Hola","Chao");
+        System.out.println(text);
+
+        System.out.println(texto.endsWith("undo!"));
+        System.out.println(texto.startsWith("Ch"));
+        System.out.println(texto.contains("und"));
+
+
+
+
     }
 }
